690c5d1c74eca15f4fcf63df748feba246c37da8
LeonLin0516 <52585348+LeonLin0516@users.noreply.github.com>
2020-12-18 17:09:47 -0800

    progress on layout

--- src/Component/Profile/Profile.js
+++ src/Component/Profile/Profile.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const Profile = () => <h1>Profile</h1>
+const Profile = () => (
+  <div>
+    <h1>Profile</h1>
+    <h2>Undergraduate Student at UCSC</h2>
+    <hr></hr>
+    <h2>About me</h2>
+    <div>Placeholder for Pic</div>
+  </div>
+);
  
 export default Profile
